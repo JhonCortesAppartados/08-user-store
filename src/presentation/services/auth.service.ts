6df0cbd20fa527d8a73062b523f2ea973eb4b39c
JhonCortesAppartados <d_junior1@appartados.com>
2025-05-21 17:06:36 -0500

    Se creo las validaciones para el login como para registar usuario, tambien se implemento la encripptacion de la contraseña

---
+++ src/presentation/services/auth.service.ts
@@ -0,0 +1,58 @@
+import { bcryptAdapter } from "../../config";
+import { UserModel } from "../../data";
+import { CustomError, LoginUserDto, RegisterUserDto, UserEntity } from "../../domain";
+
+export class AuthService {
+
+    //inyecion de dependencias | DI:
+    constructor(){}
+
+    public async registerUser(resgisterUserDto: RegisterUserDto){
+
+        const existUser = await UserModel.findOne({email: resgisterUserDto.email});
+
+        if(existUser) throw CustomError.badRequest('Email already exist');
+
+        try {
+            const user = new UserModel(resgisterUserDto);
+            
+            //Encriptar la contraseña:
+            user.password = bcryptAdapter.hash(resgisterUserDto.password);
+            
+            await user.save();
+            //JWT <---- para mantener la autenticación del usuario:
+
+            //Email de confirmación:
+            
+            //Se muestra la información del usuario sin la contraseña:
+            const {password, ...userEntity} = UserEntity.fromObject(user);
+
+            return {
+                user: userEntity,
+                token: 'ABC'
+            };
+        } catch (error) {
+            throw CustomError.internalServer(`${error}`);
+        }
+
+    }
+
+    public async loginUser(loginUserDto: LoginUserDto){
+
+        const user = await UserModel.findOne({email: loginUserDto.email});
+
+        if(!user) throw CustomError.badRequest('Email or password are wrong');
+
+        const isMatching = bcryptAdapter.compare(loginUserDto.password, user.password);
+
+        if(!isMatching) throw CustomError.badRequest('Email or password are wrong');
+
+        const {password, ...userEntity} = UserEntity.fromObject(user);
+
+        return {
+            user: userEntity,
+            token: 'ABC'
+        };
+
+    }
+}

52d5210dc010ef0f650c28e61486ca06135c0adf
JhonCortesAppartados <d_junior1@appartados.com>
2025-05-24 08:17:43 -0500

    se modifico el archivo seed para que no se ejecutara en el comando el npm run dev

--- src/data/seed/seed.ts
+++ src/data/seed/seed.ts
@@ -1,5 +1,6 @@
 import { envs } from "../../config";
-import { MongoDatabase } from "../mongo/mongo-database"
+import { CategoryModel, MongoDatabase, ProductModel, UserModel } from "../mongo";
+import { seedData } from "./data";
 
 
 (async() => {
@@ -12,12 +13,55 @@ import { MongoDatabase } from "../mongo/mongo-database"
     await main();
 
     await MongoDatabase.disconnect();
-})
+});
+
+//Esta funciones para poder generar numero aleatorios, dependiendo del rango que queremos:
+const randomBetween0AndX = (x: number) => {
+    return Math.floor(Math.random() * x);
+}
 
 
 async function main(){
 
+    //0. Limpiar la base de datos:
+    await Promise.all([
+        UserModel.deleteMany(),
+        CategoryModel.deleteMany(),
+        ProductModel.deleteMany(),
+    ])
 
+    //1. Crear los usuarios:
+    const users = await UserModel.insertMany(seedData.users);
+
+    //2. Crear las categorias:
+    const categories = await CategoryModel.insertMany(
+        seedData.categories.map(category => {
+
+            return {
+                ...category,
+                user: users[0]._id
+            }
+
+        })
+    );
+
+    //3. Crear los productos:
+    const products = await ProductModel.insertMany(
+        seedData.products.map(product => {
+
+            return {
+                ...product,
+                //Se utiliza la funcion randomBetween0AndX para poder obtener un numero aleatorio entre 0 y el largo de la base de datos:
+                //Para poder obtener un usuario aleatorio de la base de datos, y simular que difrerentes usuarios crearon varios productos en diferentes categorias:
+                user: users[randomBetween0AndX(seedData.users.length)]._id,
+                category: categories[randomBetween0AndX(seedData.categories.length)]._id
+            }
+
+        })
+    );
+    
+
+    console.log('SEEDED DATA');
 }
 
 

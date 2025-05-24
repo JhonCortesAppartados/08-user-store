9dec3f56afe5a2106149df2fac4959cea717fdf0
JhonCortesAppartados <d_junior1@appartados.com>
2025-05-24 09:17:20 -0500

    se implemento las rutas y el controlador para poder cargar los archivos, tambien se creo el servicio para poder hacer las funciones de cargar, se implemento el middleware con el paquete express-fileupload

---
+++ src/presentation/file-upload/controller.ts
@@ -0,0 +1,28 @@
+import { Response, Request } from "express";
+import { CustomError } from "../../domain";
+
+export class FileUploadController {
+
+    constructor(){}
+
+    private handleError = (error: unknown, res: Response) => {
+        if(error instanceof CustomError){ 
+            return res.status(error.statusCode).json({error: error.message});
+        }
+        console.log(`${error}`);
+        return res.status(500).json({error: 'Internal Server Error'});
+    }
+
+    uploadFile = (req: Request, res: Response) => {
+
+        console.log({files: req.files});
+
+        res.json('uploadFile');
+    }
+
+    uploadMultipleFiles = (req: Request, res: Response) => {
+
+        res.json('uploadMultipleFiles');
+    }
+
+}
---
+++ src/presentation/file-upload/routes.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import { FileUploadController } from "./controller";
+
+
+
+export class FileUploadRoutes {
+
+    static get routes(): Router {
+        
+        const router = Router();
+
+        const controller = new FileUploadController();
+        
+        // Definir las rutas:
+        //Se establecera para poder subir los diferentes archivos:
+        // api/upload/single/<user|category|product>/
+        // api/upload/multiple/<user|category|product>/
+        router.post('/single/:type', controller.uploadFile);
+        router.post('/multiple/:type', controller.uploadMultipleFiles);
+
+
+
+        return router;
+    }
+
+
+
+}
